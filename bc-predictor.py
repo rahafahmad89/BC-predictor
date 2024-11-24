35a92ed49a27c0a607f1131b683246368ef6776c
rahafahmad89 <105476503+rahafahmad89@users.noreply.github.com>
2024-11-24 20:09:11 +0400

    Update bc-predictor.py

--- bc-predictor.py
+++ bc-predictor.py
@@ -19,7 +19,8 @@ The prediction is done by an RNN model using feature importance derived from the
 
 
 # Input sliders for all features
-st.subheader("Input Variant Features")
+st.subheader("Input Variant Features", "(Move the sliders to change the scores and see how it affects the prediction!)")
+
 gerp = st.slider("GERP++ RS (0 to 5)", 0.0, 5.0, 2.5)
 phyloP = st.slider("phyloP Mammalian Score (0 to 5)", 0.0, 5.0, 2.5)
 cadd = st.slider("CADD PHRED Score (0 to 50)", 0.0, 50.0, 25.0)
